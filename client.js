f91b551beba84c621ca1970f0057e3f90611df53
Julie Zembik <kurokurosan@gmail.com>
2018-12-14 16:34:08 -0600

    formatted div IDs for better temp-viewing, added to client.js, function code to append input values into respective divs

--- client.js
+++ client.js
@@ -2,8 +2,25 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('JQ Ready!');
-    
+    $('#submit').on('click', submitClick);
 }
 
 console.log('JS Ready!');
+
+
+
+function submitClick(){
+    console.log('Button Clicked!');
+    let subFirstName = $('#firstName').val();
+    let subLastName = $('#lastName').val();
+    let subEmpId = $('#empId').val();
+    let subEmpTitle = $('#empTitle').val();
+    let subEmpSalary = $('#empSalary').val();
+
+    $('#fNameList').append(`<tr><td>${subFirstName}</td></tr>`);
+    $('#lNameList').append(`<tr><td>${subLastName}</td></tr>`);
+    $('#empIdList').append(`<tr><td>${subEmpId}</td></tr>`);
+    $('#empTitleList').append(`<tr><td>${subEmpTitle}</td></tr>`);
+    $('#empSalaryList').append(`<tr><td>${subEmpSalary}</td></tr>`);
+}
 
