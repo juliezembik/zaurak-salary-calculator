9528d0fbce789fc510acd3cf281b272218894f76
Julie Zembik <kurokurosan@gmail.com>
2018-12-14 20:19:23 -0600

    fixed code a bit, turned some individual divs into table, td, tr

--- client.js
+++ client.js
@@ -17,10 +17,10 @@ function submitClick(){
     let subEmpTitle = $('#empTitle').val();
     let subEmpSalary = $('#empSalary').val();
 
-    $('#fNameList').append(`<tr><td>${subFirstName}</td></tr>`);
-    $('#lNameList').append(`<tr><td>${subLastName}</td></tr>`);
-    $('#empIdList').append(`<tr><td>${subEmpId}</td></tr>`);
-    $('#empTitleList').append(`<tr><td>${subEmpTitle}</td></tr>`);
-    $('#empSalaryList').append(`<tr><td>${subEmpSalary}</td></tr>`);
+    $('#submittedFirstName').append(`<tr><td>${subFirstName}</td></tr>`);
+    $('#submittedLastName').append(`<tr><td>${subLastName}</td></tr>`);
+    $('#submittedEmpId').append(`<tr><td>${subEmpId}</td></tr>`);
+    $('#submittedEmpTitle').append(`<tr><td>${subEmpTitle}</td></tr>`);
+    $('#submittedEmpSalary').append(`<tr><td>${subEmpSalary}</td></tr>`);
 }
 
