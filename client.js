f4770f3216b50c09f07028b8a771676d30db2ee6
Julie Zembik <kurokurosan@gmail.com>
2018-12-26 13:21:20 -0600

    was able to get totalSalary to console log

--- client.js
+++ client.js
@@ -2,27 +2,42 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('JQ Ready!');
-    $('#submit').on('click', submitClick);
+    $('#submit').on('click', addEmployee);
 }
 
 console.log('JS Ready!');
 
 const maxSalary = 20000;
 
-function submitClick(){
-    console.log('Button Clicked!');
-    
-    let subFirstName = $('#firstName').val();
-    let subLastName = $('#lastName').val();
-    let subEmpId = $('#empId').val();
-    let subEmpTitle = $('#empTitle').val();
-    let subEmpSalary = $('#empSalary').val();
+let employees = [];
 
-    $('#submittedFirstName').append(`<tr><td>${subFirstName}</td></tr>`);
-    $('#submittedLastName').append(`<tr><td>${subLastName}</td></tr>`);
-    $('#submittedEmpId').append(`<tr><td>${subEmpId}</td></tr>`);
-    $('#submittedEmpTitle').append(`<tr><td>${subEmpTitle}</td></tr>`);
-    $('#submittedEmpSalary').append(`<tr><td>${subEmpSalary}</td></tr>`);
+function addEmployee(){
+    console.log('Button Clicked!');
+    let newEmployee = {
+    firstName: $('#firstName').val(),
+    lastName: $('#lastName').val(),
+    empId: $('#empId').val(),
+    empTitle: $('#empTitle').val(),
+    empSalary: $('#empSalary').val(),
+    }
+
+    employees.push(newEmployee);
+    console.log('adding', newEmployee);
+    
+
+    // $('#submittedFirstName').append(`<tr><td>${subFirstName}</td></tr>`);
+    // $('#submittedLastName').append(`<tr><td>${subLastName}</td></tr>`);
+    // $('#submittedEmpId').append(`<tr><td>${subEmpId}</td></tr>`);
+    // $('#submittedEmpTitle').append(`<tr><td>${subEmpTitle}</td></tr>`);
+    // $('#submittedEmpSalary').append(`<tr><td>${subEmpSalary}</td></tr>`);
+
+    let totalSalary = 0;
+    for(employee of employees){
+        totalSalary += Number(employee.empSalary);
+    }
+    console.log('total salary: ', totalSalary);
+    
+
     clearInputs();
 }
 
